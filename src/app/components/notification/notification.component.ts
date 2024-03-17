6d98344054d857fc2127286bf122d43be648bcee
Vladyslav Tochanenko <itochanenko@gmail.com>
2024-03-17 14:36:12 +0200

    Make Notifications Fade Out

--- src/app/components/notification/notification.component.ts
+++ src/app/components/notification/notification.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule, NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NotificationService } from '../../services/notification.service';
-import { Observable } from 'rxjs';
+import { Observable, mergeMap, tap } from 'rxjs';
 
 @Component({
   selector: 'notification',
@@ -13,8 +13,30 @@ import { Observable } from 'rxjs';
   templateUrl: './notification.component.html',
   styleUrl: './notification.component.scss'
 })
-export class NotificationComponent {
-  notification$: Observable<string> = this.notificationService.getNotifications$();
+export class NotificationComponent implements OnInit {
+  visible: boolean = false;
+  private visibleSeconds: number = 0;
+  private NOTIFICATION_SECONDS: number = 5;
+
+  notification$: Observable<string> = this.notificationService.getNotifications$().pipe(
+    tap({
+      next: () => this.makeVisible()
+    })
+  );
+
+  ngOnInit(): void {
+    setInterval(() => {
+      if (this.visibleSeconds > 0) {
+        this.visible = true;
+        this.visibleSeconds--;
+      } else this.visible = false;
+    }, 1000);
+  }
+
+  makeVisible(): void {
+    this.visibleSeconds = this.NOTIFICATION_SECONDS;
+    this.visible = true;
+  }
 
   constructor(private notificationService: NotificationService) {}
 }
